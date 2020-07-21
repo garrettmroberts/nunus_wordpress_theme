3eb1161aaf5161486ee85b79eb4318c93c7d3a99
Garrett Roberts <garrettmroberts@gmail.com>
2020-07-21 16:05:31 -0500

    more templating

--- front-page.php
+++ front-page.php
@@ -26,9 +26,11 @@
   </div>
 
   <div id="find-us">
-    <h3>How to find us</h3>
     <div>
-      <img src="https://via.placeholder.com/150">
+      <div id="map">
+        <h3>How to find us</h3>
+        <img src="https://via.placeholder.com/500x300?text=Map+Placeholder">
+      </div>
       <div id="contact">
         <p>Open Weekends 11AM-7PM</p>
         <p>Phone: (510)-520-0175</p>
