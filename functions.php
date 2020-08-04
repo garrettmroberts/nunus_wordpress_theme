af07f4d2564ce537b4931642b32fc6fc09aa046d
Garrett Roberts <garrettmroberts@gmail.com>
2020-08-04 11:15:57 -0500

    custom wp post type

--- functions.php
+++ functions.php
@@ -2,13 +2,37 @@
 
 // Adding the CSS and JS files
 
-function gt_setup() {
+function setup() {
   wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
   wp_enqueue_style('font-awesome', 'https://kit.fontawesome.com/3899a97913.js');
   wp_enqueue_style('style', get_stylesheet_uri(), NULL, microtime(), all);
   wp_enqueue_script('main', get_theme_file_uri('/js/main.js'), NULL, microtime(), true);
 }
 
-add_action('wp_enqueue_scripts', 'gt_setup');
+add_action('wp_enqueue_scripts', 'setup');
+
+
+// custom post type
+function custom_post_type() {
+  register_post_type('menu item',
+    array(
+      'rewrite' => array('slug' => 'menu items'),
+      'labels' => array(
+        'name' => 'Menu Items',
+        'singular_name' => 'Menu Item',
+        'add_new_item' => 'Add new Item',
+        'edit_item' => 'Edit Menu Item'
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'supports' => array(
+        'title', 'thumbnail','post-formats', 'editor', 'custom-fields'
+      )
+    )
+  );
+}
+
+add_action('init', 'custom_post_type');
+
 
 ?>
